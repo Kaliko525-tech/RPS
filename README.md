206caa077683e7b3b5e5578067ef7d78eb0db28a
kalvinwork <kalvinhamilton525@outlook.com>
2026-05-22 22:42:15 +0100

    complete task

--- README.md
+++ README.md
@@ -11,3 +11,27 @@ I will be starting with making a function called computersChoice
 the computer will randomly choose between "rock" "paper" "scissors" to do this I will need to have a random number generated and depeding on that number the function will choose one of the three choices. 
 
 looking at the documentation for math.random() i only want it to go up to 3 numbers, so i can just use the basic 1 parameter. I will also use a default to of 3 for this when setting the max parameter. 
+
+okay thats done and was pretty easy
+
+now we are onto the harder part lets see how we go. 
+
+okay so a round of RPS can go the following way
+
+rock beats scissors
+paper beats rock
+scissors beats paper
+
+human == Paper && compuer == rock - human win // humanscore++
+
+human == rock && computer == scissors - human win // humanscore++
+
+human == scissors && computer == paper - human win  // humanscore++
+
+human == rock && computer == paper - computer win  // computerScore++
+
+human == scissors && computer == rock - computer win // computerScore++
+
+human == paper && computer == scissors - computer win // computerScore++
+
+human == paper && computer == paper || human == scissors && computer == scissors || human == rock && computer == rock - Its a draw score != ++
--- scripts.js
+++ scripts.js
@@ -1,30 +1,84 @@
 console.log("hello world!")
 
 let humanScore = 0
-let computerScore = 0
+    let computerScore = 0
 
 function getComputerChoice() {
     value = Math.floor(Math.random() * 3);
 
     if (value == 0) {
-         computerChoice = "Rock"
-        console.log(computerChoice)
+        computerChoice = "Rock"
+      return computerChoice
     } 
     else if (value == 1) {
         computerChoice = "Paper"
-        console.log(computerChoice)
+      return computerChoice
+        
     }
     else if (value == 2) {
         computerChoice = "Scissors"
-        console.log(computerChoice)
+      return computerChoice
+        
     }
     else console.log("Error")
 }
 
 function getHumanChoice() {
     userChoice = prompt("Rock, Paper, or Scissors" , )
-    console.log(userChoice)
+   return userChoice = userChoice.toLowerCase()
 }
 
-getComputerChoice()
-getHumanChoice()
+
+function playRound(humanChoice , computerChoice) {
+    if (humanChoice == "paper" && computerChoice == "Rock") {
+        console.log("Paper beats Rock! Human Wins!")
+        humanScore = ++humanScore 
+        console.log(humanScore , computerScore , humanChoice, computerChoice)
+    }
+    else if (humanChoice == "rock" && computerChoice == "Scissors") {
+        console.log("Rock beats Scissors! Human Wins!")
+        humanScore = ++humanScore 
+        console.log(humanScore , computerScore , humanChoice, computerChoice)
+    }
+    else if (humanChoice == "scissors" && computerChoice == "Paper") {
+        console.log("Scissors beats Paper! Human Wins!")
+        humanScore = ++humanScore 
+        console.log(humanScore , computerScore , humanChoice, computerChoice)
+    }
+    else if (humanChoice == "rock" && computerChoice == "Paper") {
+        console.log("Rock loses to Paper! Computer Wins!")
+        computerScore = ++computerScore 
+        console.log(humanScore , computerScore , humanChoice, computerChoice)
+    }
+    else if (humanChoice == "scissors" && computerChoice == "Rock") {
+        console.log("Scissors loses to Rock! Computer Wins!")
+        computerScore = ++computerScore 
+        console.log(humanScore , computerScore , humanChoice, computerChoice)
+    }
+    else if (humanChoice == "paper" && computerChoice == "Scissors") {
+        console.log("Paper loses to Scissors! Computer Wins!")
+        computerScore = ++computerScore ;
+        console.log(humanScore , computerScore , humanChoice, computerChoice)
+    }
+    else if (humanChoice == "paper" && computerChoice == "Paper" || humanChoice == "rock" && computerChoice == "Rock" || humanChoice == "scissors" && computerChoice == "Scissors") {
+        console.log("ITS A DRAW!")
+        console.log(humanScore , computerScore , humanChoice, computerChoice)
+    }
+} 
+
+
+
+
+
+function playGame(){
+    const humanSelection = getHumanChoice()
+    const computerSelection = getComputerChoice()
+
+    playRound(humanSelection , computerSelection)
+}
+
+playGame()
+playGame()
+playGame()
+playGame()
+playGame()
